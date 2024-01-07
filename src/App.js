e05f7e14eab32377cc75b77054b76b7843807908
GeeGrrit <gerrit301@gmail.com>
2024-01-07 14:08:47 +0100

    Add function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+
+//Json File later
+const data = [
+  { B: "Big test with some stuff", I: "19", N: "TEST",G: "34",O: "2" },
+  { B: "Big test with some stuff", I: "19", N: "TEST",G: "34",O: "2" },
+  { B: "Big test with some stuff", I: "19", N: "Big test with some stuff",G: "34",O: "2" },
+  { B: "Big test with some stuff", I: "19", N: "TEST",G: "34",O: "2" },
+  { B: "Big test with some stuff And so on and so so so sos sososo", I: "19", N: "Wow",G: "Big test with some stuff",O: "2" },
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tr>
+          <th>B</th>
+          <th>I</th>
+          <th>N</th>
+          <th>G</th>
+          <th>O</th>
+        </tr>
+        {data.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.B}</td>
+              <td>{val.I}</td>
+              <td>{val.N}</td>
+              <td>{val.G}</td>
+              <td>{val.O}</td>
+            </tr>
+          )
+        })}
+    </table>
     </div>
   );
 }
